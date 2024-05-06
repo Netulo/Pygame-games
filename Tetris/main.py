0809b1da982b108b0d2443f1fffa15c5f99e9fce
Netulo <matteu06@gmail.com>
2024-05-06 21:51:08 +0200

    Tetris - game logic

---
+++ Tetris/main.py
@@ -0,0 +1,38 @@
+import pygame
+from game import Game
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Tetris")
+clock = pygame.time.Clock()
+
+dark_blue = (44, 44, 127)
+
+game = Game()
+
+GAME_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(GAME_UPDATE, 100)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and game.game_over == False:
+                game.move_left()
+            elif event.key == pygame.K_RIGHT and game.game_over == False:
+                game.move_right()
+            elif event.key == pygame.K_DOWN and game.game_over == False:
+                game.move_down()
+            elif event.key == pygame.K_UP and game.game_over == False:
+                game.rotate()
+            elif game.game_over == True:
+                game.game_over = False
+                game.reset()
+        if event.type == GAME_UPDATE and game.game_over == False:
+            game.move_down()
+            
+    screen.fill(dark_blue)
+    game.draw(screen)
+    
+    pygame.display.update()
+    clock.tick(60)
