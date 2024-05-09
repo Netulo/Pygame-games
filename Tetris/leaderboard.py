3752e09c6c49d985f25b50c873a914a06ac3e3bc
Netulo <matteu06@gmail.com>
2024-05-09 11:03:04 +0200

    .

--- Tetris/leaderboard.py
+++ Tetris/leaderboard.py
@@ -9,8 +9,6 @@ class Leaderboard:
         self.font = pygame.font.SysFont('Comic Sans MS', 30)
         file.close()
         
-        self.current_player = "Gacek"
-        
     def draw(self, screen):
         self.leaderboard_json = dict(sorted(self.leaderboard_json.items(), key=lambda item: item[1], reverse=True))
         counter = 0
@@ -21,6 +19,8 @@ class Leaderboard:
                     text_surface = self.font.render(f'{key}: {value}', False, (0, 0, 0))
                     screen.blit(text_surface, (350, 250 + offset_y * counter))
                     counter += 1
+                    if counter > 10:
+                        break
             if(counter <= 10):
                 text_surface = self.font.render('...: ...', False, (0, 0, 0))
                 screen.blit(text_surface, (350, 250 + offset_y * counter))
